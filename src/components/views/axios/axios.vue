9effa39635939cefd967d55e183daf5bae5212b2
tao123456789 <2413629661@qq.com>
2021-04-01 23:21:10 +0800

    2021年4月1号 自己的功能

--- src/components/views/axios/axios.vue
+++ src/components/views/axios/axios.vue
@@ -3,29 +3,29 @@
 
 
     <div>
-      <label>获取文字，示例：https://autumnfish.cn/api/joke/list?num=6</label>
-    <br>
+      <label>获取一般axios格式</label><br>
+      <label>示例：https://autumnfish.cn/api/joke/list?num=6</label><br>
     <input type="text" value="" placeholder="请输入地址" id="url" style="width: 80%;height: 30px;">
       <el-button value="获取数据" @click="getInfo" type="primary">获取数据</el-button>
       <br>
       <label :model="rs" style="background-color:	#E0E0E0">{{rs}}</label>
     </div>
     <br>
-<!--    //获取JSON数据暂时未做
+
     <div>
       <label>获取json数据</label><br>
-      <input type="text" value="" placeholder="请输入地址" id="getJSON" style="width: 80%;height: 30px;">
-      <el-button value="获取数据" @click="getJSON" type="primary">获取数据</el-button>
+      <input type="text" value="" placeholder="请输入地址" id="JSONurl" style="width: 80%;height: 30px;">
+      <el-button value="获取数据" @click="getJSO" type="primary">获取数据</el-button>
       <br>
-      <label :model="getJSON" style="background-color:	#E0E0E0">{{geJSON}}</label>
+      <label :model="JSONvalue" style="background-color:	#E0E0E0">{{JSONvalue}}</label>
     </div>
-    <br>-->
+    <br>
 
     <div>
       <label>获取图片</label><br>
       <label>示例1：http://p19.qhimg.com/bdm/1024_768_80/t0151ad4ac41239cb58.jpg</label><br>
       <label>示例2：http://cn.bing.com/th?id=OHR.OwlSunflowers_ZH-CN8154999485_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp</label><br>
-      <label>示例3：https://api.xygeng.cn/Bing/</label>
+      <label>示例3：https://api.xygeng.cn/Bing/</label><br>
       <input type="text" value="" placeholder="请输入地址" id="PicUrl" style="width: 80%;height: 30px;">
       <el-button value="获取数据" @click="getPic" type="primary">获取图片</el-button>
       <br>
@@ -43,6 +43,7 @@ export default {
       rs:"在这显示数据",
       picUrl:"在这显示图片",
       Pictureshow:false,
+      JSONvalue:"在这显示数据",
     }
   },
   methods:{
@@ -54,7 +55,7 @@ export default {
           .then(res => {
             console.log("获取url的数据成功");
             console.log(res)
-            this.rs=res.data;
+            this.rs=res;
           })
           .catch(err => {
             console.error(err);
@@ -69,13 +70,8 @@ export default {
       axios.get(url,{
         responseType:'blob',
         headers:self.header,
-/*            {
-        'Content-Type': 'application/json',
-        'data-type': 'Buffer'},
-        data:JSON.stringify(),*/
       })
           .then(res => {
-            console.log("获取url的数据成功");
             console.log(res)
             if(res.data==''){
               this.Pictureshow=false;
@@ -91,6 +87,22 @@ export default {
             this.picUrl="";
           })
     },
+    //获取JSON文件
+    getJSO:function(){
+      let getJSO=document.getElementById("JSONurl").value;
+      axios.get(getJSO,{
+        responseType:'json',
+        headers:self.header,
+      })
+          .then(res => {
+            console.log(res)
+            this.JSONvalue=res;
+          })
+          .catch(err => {
+            console.error(err);
+            this.JSONvalue=err;
+          })
+    }
   }
 }
 </script>
